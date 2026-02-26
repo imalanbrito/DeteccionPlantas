7f0d62e40f8f99f2c7a22cf4f8a20120f6dc3bd2
Alan <alan.prez1610@gmail.com>
2026-02-26 13:27:09 -0600

    verficacion corregida

--- verificar_fotos.py
+++ verificar_fotos.py
@@ -1,20 +1,17 @@
 import cv2
 import os
 
-#asignamos ruta del dataset
-ruta = 'dataset/entrenamiento/brocoli_sano'\
+# Asegúrate de que la ruta tenga las comillas de cierre y el paréntesis
+ruta = 'dataset/entrenamiento/brocoli_sano'
 archivos = os.listdir(ruta)
 
 for nombre in archivos:
-	img_path = os.path/join(ruta. nombre)
-	frame = cv2.imread(img_path)
-
-	if frame is not None:
-		cv2.putText(frame, "PROCESANDO DATOS..". (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0,0), 2)
-		CV2.imshow('Verificacion de Imagenes', frame)
-
-	#espera cualquier boton para ver la siguiente, 'q' para salir
-	if cv2.waitKey(500) & 0xFF == ord('q'):
-		break
+    img_path = os.path.join(ruta, nombre)
+    frame = cv2.imread(img_path)
+    
+    if frame is not None:
+        cv2.imshow('Verificacion', frame)
+        if cv2.waitKey(500) & 0xFF == ord('q'):
+            break
 
 cv2.destroyAllWindows()
